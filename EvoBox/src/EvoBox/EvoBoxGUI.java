bcccdd1e633ac04bee70073c8a0d18965c96f625
superhempfi <henry.feucht@icloud.com>
2024-06-06 08:35:20 +0200

    Slime struggle is real

--- EvoBox/src/EvoBox/EvoBoxGUI.java
+++ EvoBox/src/EvoBox/EvoBoxGUI.java
@@ -2,16 +2,25 @@ package EvoBox;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Arrays;
 
 public class EvoBoxGUI extends JPanel {
     private JPanel mainPanel;
     private JPanel gamePanel;
+    private JButton moveButton;
+    private Timer globalTimer;
 
     private int anzFood = 0;
     private int maxFood = 10;
     private food[] allFood = new food[maxFood];
+
+    private int anzSlimes = 0;
+    private int currentSlimes = 0;
+    private slime[] allSlimes = new slime[100];
 
     public EvoBoxGUI() {
         mainPanel = new JPanel();
@@ -25,12 +34,45 @@ public class EvoBoxGUI extends JPanel {
         gamePanel.setPreferredSize(new Dimension(1200,800));
         mainPanel.add(gamePanel);
 
+        moveButton = new JButton("Move Slime");
+        moveButton.setBounds(10, 10, 120, 30);
+        moveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (anzSlimes > 0) {
+                    int index = (int) (Math.random() * anzSlimes);
+                    int newX = (int) (Math.random() * (gamePanel.getWidth() - 100));
+                    int newY = (int) (Math.random() * (gamePanel.getHeight() - 100));
+                    allSlimes[index].startMove(newX, newY, 2000);  // Move over 2 seconds
+                }
+            }
+        });
+        mainPanel.add(moveButton);
+
+
+        globalTimer = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(int i = 0; i < anzSlimes; i++) {
+                    allSlimes[i].updatePosition();
+                }
+                gamePanel.repaint();
+            }
+        });
+
+        globalTimer.start();
+
+
+
+
+
+
 
         // Fruit wird geladen
         // i = 0 ; i < 10 -> 10 mal geladen
 
         // Anzahl von spawns
-        int toSpawn = 1;
+        int toSpawn = 10;
 
         for (int i = 0; i < toSpawn; i++) {
             int x = (int) (Math.random() * (gamePanel.getWidth() - 75));
@@ -39,6 +81,14 @@ public class EvoBoxGUI extends JPanel {
         }
 
         loadSlimes(50, 200, 64);
+
+        System.out.println(Arrays.toString(allFood));
+
+        System.out.println(allFood[0].getX());
+
+
+        allSlimes[0].startMove(allFood[0].getX(), allFood[0].getY(), 2000);
+
 
 
     }
@@ -62,6 +112,7 @@ public class EvoBoxGUI extends JPanel {
 
         allFood[anzFood] = aFood;
         gamePanel.add(aFood);
+        anzFood++;
 
     }
 
@@ -79,7 +130,9 @@ public class EvoBoxGUI extends JPanel {
 
         slime aSlime = new slime(x, y, width, height, gamePanel.getWidth(), gamePanel.getHeight(), scaledSlimeIcon);
 
+        allSlimes[currentSlimes] = aSlime;
         gamePanel.add(aSlime);
+        currentSlimes++;
 
     }
 
--- EvoBox/src/EvoBox/slime.java
+++ EvoBox/src/EvoBox/slime.java
@@ -3,12 +3,16 @@ package EvoBox;
 import javax.swing.*;
 
 public class slime extends JLabel {
-
     protected int x, y;
     protected int fw, fh;
     protected int w, h;
+    private int startX, startY;
+    private int targetX, targetY;
+    private long startTime;
+    private long duration;
+    private boolean isMoving;
 
-    public slime(int x, int y, int w, int h, int fw, int fh, ImageIcon slime){
+    public slime(int x, int y, int w, int h, int fw, int fh, ImageIcon slime) {
         super();
         this.x = x;
         this.y = y;
@@ -18,8 +22,32 @@ public class slime extends JLabel {
         this.fh = fh;
         this.setIcon(slime);
         this.setBounds(x, y, w, h);
-
     }
 
+    public void startMove(int targetX, int targetY, long duration) {
+        this.startX = this.x;
+        this.startY = this.y;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.duration = duration;
+        this.startTime = System.currentTimeMillis();
+        this.isMoving = true;
+    }
 
+    public void updatePosition() {
+        if (!isMoving) return;
+
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        double fraction = (double) elapsedTime / duration;
+        if (fraction >= 1.0) {
+            fraction = 1.0;
+            isMoving = false;
+        }
+
+        int currentX = (int) (startX + fraction * (targetX - startX));
+        int currentY = (int) (startY + fraction * (targetY - startY));
+        this.setBounds(currentX, currentY, w, h);
+        this.x = currentX;
+        this.y = currentY;
+    }
 }
